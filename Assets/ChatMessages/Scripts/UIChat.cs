260b63452236f0a0d8f3cd55b55166ba6ae1c32b
DeskTop\Ji Hwan <2bunch0@gmail.com>
2016-10-30 23:10:19 +0900

    string 배열 수정

--- Assets/ChatMessages/Scripts/UIChat.cs
+++ Assets/ChatMessages/Scripts/UIChat.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-	//------수정 시간: 2016년 10월 28일--------//
+	//------수정 시간: 2016년 10월 30일--------//
 
 
 public class UIChat : MonoBehaviour
@@ -25,8 +25,22 @@ public class UIChat : MonoBehaviour
 	[Header("_bot.Reply를 사용하기 위한 class형")]
 	[SerializeField] private UIChatBot _bot;
 
-    private List<UIChatMessageItem> _messageItems;
+	[Header("플레이어 메시지 스트링")]
+	public string[] Player_Messages = new string[3];
+
+	private List<UIChatMessageItem> _messageItems;
 	//------------EVENTMETHOD-----------------//
+	//------------EVENTMETHOD-----------------//
+	void Start()
+	{
+		//CSVReader 클래스가 test2를 읽었을 때의 Dictionary값을 가져옵니다.
+		List<Dictionary<string, object>> Dic_CSV_Data = CSVReader.Read("test2");
+
+		for (int i = 0; i < 3; i++)
+		{
+			Player_Messages[i] = Dic_CSV_Data[i+10]["Log"].ToString();
+		}
+	}
 	void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Return))
@@ -34,13 +48,37 @@ public class UIChat : MonoBehaviour
 	}
 	//--------------METHOD--------------------//
 	/// <summary>
+	/// Input field가 공란이 아닐 때 쓰인 메시지를 보냅니다.
+	/// </summary>
+	private void SendMessage()
+	{
+		string message = _inputField.text;
+
+		if (!string.IsNullOrEmpty(message))
+		{
+			AddMessage(message, true);
+
+			_inputField.text = "";
+		}
+	}
+	#region UI EVENT Send Message      
+	/// <summary>
+	/// UI이벤트: 메시지 보내기 버튼이 부르는 메소드
+	/// </summary>
+	public void OnClickSendMessageButton()
+	{
+		SendMessage();
+	}
+	#endregion
+
+	/// <summary>
 	/// 메시지 아이템에 UI를 추가합니다.
 	/// </summary>
-	/// <param name="message">The message to add</param>
-	/// <param name="playerMessage">If the messsage is from the player</param>
+	/// <param name="message">추가할 메시지</param>
+	/// <param name="playerMessage">플레이어에게서 온 메시지인지 아닌지</param>
 	public void AddMessage(string message, bool playerMessage)
     {
-        UIChatMessageItem messageItem = CreateMessageItem(_vLayout, playerMessage);
+        UIChatMessageItem messageItem =  CreateMessageItem(_vLayout, playerMessage);
         messageItem.SetMessage(message);
 
 		//_messageItems이 null일때 초기화
@@ -53,18 +91,19 @@ public class UIChat : MonoBehaviour
             _bot.Reply(message);
     }
 
-    /// <summary>
-    /// 레이아웃에 새로운 메시지를 생성하고 넣습니다.
-    /// </summary>
-    /// <param name="vLayout">The Vertical Layout to add the message item.</param>
-    /// <param name="playerMessage">If the message is from the player.</param>
-    /// <returns></returns>
-    private UIChatMessageItem CreateMessageItem(VerticalLayoutGroup vLayout, bool playerMessage)
+	/// <summary>
+	/// 메시지를 생성하고 레이아웃에 넣는 함수
+	/// </summary>
+	/// <param name="vLayout">메시지 항목을 추가하기 위한 The Vertical Layout .</param>
+	/// <param name="playerMessage">플레이어에게서 온 메시지인지 아닌지.</param>
+	/// <returns></returns>
+	private UIChatMessageItem CreateMessageItem(VerticalLayoutGroup vLayout, bool playerMessage)
     {
-		//playerMessage이 참일때 _messageItemPlayerPrefab.gameObject, 거짓일 때 _messageItemOtherPrefab.gameObject를 prefab에 넣는다.
+		//플레이어가 보내는거면 _messageItemPlayerPrefab.gameObject, 
+		//플레이어가 보내는게 아니면 _messageItemOtherPrefab.gameObject를 prefab에 넣는다.
 		GameObject prefab = playerMessage ? _messageItemPlayerPrefab.gameObject : _messageItemOtherPrefab.gameObject;
 
-		//위의 prefab을 position rotation 0 0 0으로 하는 instance를 생성합니다.
+		//위의 prefab을 position rotation 0 0 0으로 하는 instance를 생성한다.
         GameObject instance = (GameObject)Instantiate(prefab, Vector3.zero, Quaternion.identity);
 
 		//생성한 instance의 부모를 transform형 vLayout을 둡니다.
@@ -76,28 +115,4 @@ public class UIChat : MonoBehaviour
 		//instance가 가지고 있는 UIChatMessageItem 컴포넌트를 반환합니다.
 		return instance.GetComponent<UIChatMessageItem>();
     }
-
-    /// <summary>
-    /// Input field가 공란이 아닐 때 쓰인 메시지를 보냅니다.
-    /// </summary>
-    private void SendMessage()
-    {
-        string message = _inputField.text;
-
-        if (!string.IsNullOrEmpty(message))
-        {
-            AddMessage(message, true);
-
-            _inputField.text = "";
-        }
-    }
-    #region UI EVENT Send Message      
-    /// <summary>
-    /// UI이벤트: 메시지 보내기 버튼이 부르는 메소드
-    /// </summary>
-    public void OnClickSendMessageButton()
-    {
-        SendMessage();
-    }
-    #endregion
 }
--- Assets/ChatMessages/Scripts/UIChatBot.cs
+++ Assets/ChatMessages/Scripts/UIChatBot.cs
@@ -3,7 +3,7 @@ using System.Collections;
 //리스트를 사용하기 위한 namespace
 using System.Collections.Generic;
 
-	//------수정 시간: 2016년 10월 28일--------//
+	//------수정 시간: 2016년 10월 30일--------//
 
 public class UIChatBot : MonoBehaviour
 {
@@ -13,8 +13,8 @@ public class UIChatBot : MonoBehaviour
 	//UIChat.AddMessage 메소드를 사용하기 위한 _chat
 	[SerializeField] private UIChat _chat;
 
-	//Bot이 말하는 내용을 담는 string 배열
-    public string[] Messages = new string[4];
+	[Header("NPC 메시지 스트링")]
+	public string[] Npc_Messages = new string[5];
 
 	//메시지를 몇개나 보냈는지 확인하기 위한 int값 정의
     private int _currentMessage = -1;
@@ -27,9 +27,9 @@ public class UIChatBot : MonoBehaviour
 		//CSVReader 클래스가 test2를 읽었을 때의 Dictionary값을 가져옵니다.
 		List<Dictionary<string, object>> Dic_CSV_Data = CSVReader.Read("test2");
 
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 5; i++)
 		{
-			Messages[i] = Dic_CSV_Data[i]["Log"].ToString();
+			Npc_Messages[i] = Dic_CSV_Data[i+5]["Log"].ToString();
 		}
 	}
 
@@ -41,28 +41,10 @@ public class UIChatBot : MonoBehaviour
 	/// <param name="message"></param>
 	public void Reply(string message)
     {
-        if (!offline)
-        {
-			//_currentMessage = -1, 4보다 작을 때
 			if (_currentMessage < 4)
-            {
-				//현재 int값 추가
-                _currentMessage++;
-            }
-            else
-            {
-				//_currentMessage가 4고, 플레이어가 'hi'라고 말할 떄
-				if (message.ToLower() == "hi")
-                {
-					//int 메시지 값 추가
-                    _currentMessage++;
-					//상대 NPC가 답장을 하지 않게 된다.
-                    offline = true;
-                }
-            }
-
-            StartCoroutine(CoReply(Messages[_currentMessage]));
-        }
+				_currentMessage++;
+            
+            StartCoroutine(CoReply(Npc_Messages[_currentMessage]));
     }
 
     /// <summary>
@@ -72,8 +54,33 @@ public class UIChatBot : MonoBehaviour
     /// <returns></returns>
     private IEnumerator CoReply(string message)
     {
-        yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+        yield return new WaitForSeconds(1f);
 
         _chat.AddMessage(message, false);	
     }
+	//public void 백업Reply(string message)
+	//{
+	//	if (!offline)
+	//	{
+	//		//_currentMessage = -1, 4보다 작을 때
+	//		if (_currentMessage < 4)
+	//		{
+	//			//현재 int값 추가
+	//			_currentMessage++;
+	//		}
+	//		else
+	//		{
+	//			//_currentMessage가 4고, 플레이어가 'hi'라고 말할 떄
+	//			if (message.ToLower() == "hi")
+	//			{
+	//				//int 메시지 값 추가
+	//				_currentMessage++;
+	//				//상대 NPC가 답장을 하지 않게 된다.
+	//				offline = true;
+	//			}
+	//		}
+
+	//		StartCoroutine(CoReply(Npc_Messages[_currentMessage]));
+	//	}
+	//}
 }
