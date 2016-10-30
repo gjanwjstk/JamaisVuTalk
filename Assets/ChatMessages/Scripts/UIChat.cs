2fe5931a40e7bb8b60b2493dcad4c0d7e3a0b3f4
DeskTop\Ji Hwan <2bunch0@gmail.com>
2016-10-30 20:58:33 +0900

    유니티 파일 업로드

---
+++ Assets/ChatMessages/Scripts/UIChat.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+//리스트를 위한 namespace
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+	//------수정 시간: 2016년 10월 28일--------//
+
+
+public class UIChat : MonoBehaviour
+{
+	//--------------FIELD---------------------//
+
+	[Header("플레이어 메시지 프리팹")]
+	[SerializeField] private UIChatMessageItem _messageItemPlayerPrefab;
+	[Header("NPC 메시지 프리팹")]
+	[SerializeField] private UIChatMessageItem _messageItemOtherPrefab;
+
+	[Header("메시지를 자식으로 두는 레이아웃 프리팹")]
+    [SerializeField] private VerticalLayoutGroup _vLayout;
+
+	[Header("내가 쓰는 부분을 생성하는 InputField")]
+	[SerializeField] private InputField _inputField;
+
+	[Header("_bot.Reply를 사용하기 위한 class형")]
+	[SerializeField] private UIChatBot _bot;
+
+    private List<UIChatMessageItem> _messageItems;
+	//------------EVENTMETHOD-----------------//
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+			SendMessage();
+	}
+	//--------------METHOD--------------------//
+	/// <summary>
+	/// 메시지 아이템에 UI를 추가합니다.
+	/// </summary>
+	/// <param name="message">The message to add</param>
+	/// <param name="playerMessage">If the messsage is from the player</param>
+	public void AddMessage(string message, bool playerMessage)
+    {
+        UIChatMessageItem messageItem = CreateMessageItem(_vLayout, playerMessage);
+        messageItem.SetMessage(message);
+
+		//_messageItems이 null일때 초기화
+		if (_messageItems == null)
+            _messageItems = new List<UIChatMessageItem>();
+
+        _messageItems.Add(messageItem);
+
+        if (playerMessage)
+            _bot.Reply(message);
+    }
+
+    /// <summary>
+    /// 레이아웃에 새로운 메시지를 생성하고 넣습니다.
+    /// </summary>
+    /// <param name="vLayout">The Vertical Layout to add the message item.</param>
+    /// <param name="playerMessage">If the message is from the player.</param>
+    /// <returns></returns>
+    private UIChatMessageItem CreateMessageItem(VerticalLayoutGroup vLayout, bool playerMessage)
+    {
+		//playerMessage이 참일때 _messageItemPlayerPrefab.gameObject, 거짓일 때 _messageItemOtherPrefab.gameObject를 prefab에 넣는다.
+		GameObject prefab = playerMessage ? _messageItemPlayerPrefab.gameObject : _messageItemOtherPrefab.gameObject;
+
+		//위의 prefab을 position rotation 0 0 0으로 하는 instance를 생성합니다.
+        GameObject instance = (GameObject)Instantiate(prefab, Vector3.zero, Quaternion.identity);
+
+		//생성한 instance의 부모를 transform형 vLayout을 둡니다.
+        instance.transform.SetParent(vLayout.transform);
+		//instance의 localScale을 1,1,1로 맞춥니다.
+        instance.transform.localScale = Vector3.one;
+        instance.SetActive(true);
+
+		//instance가 가지고 있는 UIChatMessageItem 컴포넌트를 반환합니다.
+		return instance.GetComponent<UIChatMessageItem>();
+    }
+
+    /// <summary>
+    /// Input field가 공란이 아닐 때 쓰인 메시지를 보냅니다.
+    /// </summary>
+    private void SendMessage()
+    {
+        string message = _inputField.text;
+
+        if (!string.IsNullOrEmpty(message))
+        {
+            AddMessage(message, true);
+
+            _inputField.text = "";
+        }
+    }
+    #region UI EVENT Send Message      
+    /// <summary>
+    /// UI이벤트: 메시지 보내기 버튼이 부르는 메소드
+    /// </summary>
+    public void OnClickSendMessageButton()
+    {
+        SendMessage();
+    }
+    #endregion
+}
---
+++ Assets/ChatMessages/Scripts/UIChatBot.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+//리스트를 사용하기 위한 namespace
+using System.Collections.Generic;
+
+	//------수정 시간: 2016년 10월 28일--------//
+
+public class UIChatBot : MonoBehaviour
+{
+	//--------------FIELD---------------------//
+	
+
+	//UIChat.AddMessage 메소드를 사용하기 위한 _chat
+	[SerializeField] private UIChat _chat;
+
+	//Bot이 말하는 내용을 담는 string 배열
+    public string[] Messages = new string[4];
+
+	//메시지를 몇개나 보냈는지 확인하기 위한 int값 정의
+    private int _currentMessage = -1;
+
+    private bool offline;
+
+	//------------EVENTMETHOD-----------------//
+	void Start()
+	{
+		//CSVReader 클래스가 test2를 읽었을 때의 Dictionary값을 가져옵니다.
+		List<Dictionary<string, object>> Dic_CSV_Data = CSVReader.Read("test2");
+
+		for (int i = 0; i < 4; i++)
+		{
+			Messages[i] = Dic_CSV_Data[i]["Log"].ToString();
+		}
+	}
+
+	//--------------METHOD--------------------//
+
+	/// <summary>
+	/// Add a new message to the chat from the bot.봇이 가지고 
+	/// </summary>
+	/// <param name="message"></param>
+	public void Reply(string message)
+    {
+        if (!offline)
+        {
+			//_currentMessage = -1, 4보다 작을 때
+			if (_currentMessage < 4)
+            {
+				//현재 int값 추가
+                _currentMessage++;
+            }
+            else
+            {
+				//_currentMessage가 4고, 플레이어가 'hi'라고 말할 떄
+				if (message.ToLower() == "hi")
+                {
+					//int 메시지 값 추가
+                    _currentMessage++;
+					//상대 NPC가 답장을 하지 않게 된다.
+                    offline = true;
+                }
+            }
+
+            StartCoroutine(CoReply(Messages[_currentMessage]));
+        }
+    }
+
+    /// <summary>
+    /// Wait a random time before sending the reply.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    private IEnumerator CoReply(string message)
+    {
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+
+        _chat.AddMessage(message, false);	
+    }
+}
